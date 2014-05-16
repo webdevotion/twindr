7cd35ae730adcf9b25d41adeaaca07904956b415
Krzysztof Zablocki <krzysztof.zablocki@me.com>
2014-05-16 11:34:33 +0200

    initial setup

---
+++ Pods/Pods-twindr-environment.h
@@ -0,0 +1,79 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 8
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 1
+
+// CocoaLumberjack/Core
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Core
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Core 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Core 8
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Core 1
+
+// CocoaLumberjack/Extensions
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Extensions
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Extensions 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 8
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 1
+
+// KZAsserts
+#define COCOAPODS_POD_AVAILABLE_KZAsserts
+#define COCOAPODS_VERSION_MAJOR_KZAsserts 1
+#define COCOAPODS_VERSION_MINOR_KZAsserts 0
+#define COCOAPODS_VERSION_PATCH_KZAsserts 0
+
+// NSLogger
+#define COCOAPODS_POD_AVAILABLE_NSLogger
+#define COCOAPODS_VERSION_MAJOR_NSLogger 1
+#define COCOAPODS_VERSION_MINOR_NSLogger 2
+#define COCOAPODS_VERSION_PATCH_NSLogger 0
+
+// NSLogger-CocoaLumberjack-connector
+#define COCOAPODS_POD_AVAILABLE_NSLogger_CocoaLumberjack_connector
+#define COCOAPODS_VERSION_MAJOR_NSLogger_CocoaLumberjack_connector 1
+#define COCOAPODS_VERSION_MINOR_NSLogger_CocoaLumberjack_connector 3
+#define COCOAPODS_VERSION_PATCH_NSLogger_CocoaLumberjack_connector 0
+
+// dyci
+#define COCOAPODS_POD_AVAILABLE_dyci
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.5.20140124.
+
+// dyci/Core
+#define COCOAPODS_POD_AVAILABLE_dyci_Core
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.5.20140124.
+
+// dyci/Helpers
+#define COCOAPODS_POD_AVAILABLE_dyci_Helpers
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.5.20140124.
+
+// dyci/Injections
+#define COCOAPODS_POD_AVAILABLE_dyci_Injections
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.5.20140124.
+
+// dyci/UIKit Support
+#define COCOAPODS_POD_AVAILABLE_dyci_UIKit_Support
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.5.20140124.
+
---
+++ twindr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twindr
+//
+//  Created by Krzysztof Zabłocki on 16/05/2014.
+//  Copyright (c) 2014 webdevotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
