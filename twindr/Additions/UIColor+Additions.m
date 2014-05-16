02c8c456653e60ae459e31fadf10a906849c9a85
webdevotion <webdevotion@gmail.com>
2014-05-16 14:53:38 +0200

    added uicolor category for app colors

---
+++ twindr/Additions/UIColor+Additions.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+Additions.m
+//  Twindr
+//
+//  Created by Bram Plessers on 16/05/14.
+//  Copyright (c) 2014 webdevotion. All rights reserved.
+//
+
+#import "UIColor+Additions.h"
+
+@implementation UIColor (Additions)
+
++ (UIColor *) twindrTintColor
+{
+  return [UIColor colorWithRed:255.0f/255.0f green:45.0f/255.0f blue:85.0f/255.0f alpha:1.f];
+}
+
+@end
