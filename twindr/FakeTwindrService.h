7b34d263136e3a52e235689542c0a2e39bed6dc5
Robert Wijas <robert.wijas@gmail.com>
2014-05-16 14:03:24 +0200

    User protocol and fake service.

---
+++ twindr/FakeTwindrService.h
@@ -0,0 +1,10 @@
+//
+// Copyright 2014 webdevotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TwindrService.h"
+
+
+@interface FakeTwindrService : NSObject <TwindrService>
+@end
---
+++ twindr/TwindrUser.h
@@ -0,0 +1,11 @@
+//
+// Copyright 2014 webdevotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TwindrUser <NSObject>
+
+@property (nonatomic, readonly) NSString *username;
+
+@end
